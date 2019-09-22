cad285277fa2ea02abc170b9912e226fbdd7b446
Ralf Kehl <kehl77@posteo.de>
2019-09-22 17:21:28 +0200

    There are the basics

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import 'dotenv/config';
+import { saySomething } from './my-other-file';
+
+
+console.log(`Example app listen on port ${process.env.PORT} for inuk!`);
+console.log(`Èxample app saySomething: ${saySomething()}`);
---
+++ src/my-other-file.js
@@ -0,0 +1,9 @@
+
+import 'dotenv/config';
+
+const saySomething = () => {
+    const home = 'Beta 1';
+    return (`I come from the ${home}`);
+}
+
+export { saySomething };
